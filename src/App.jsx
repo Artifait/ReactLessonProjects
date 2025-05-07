39285ab5bad9bc0c143a9ecb9ef5f09b250e58ee
Artifait <am3044288@gmail.com>
2025-05-07 16:28:38 +0700

    End

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid';
 import './App.css'
+import MyBtn from './components/MyBtn'
+import MyCounter from './components/MyCounter'
+
+const btnContents = [
+  ['Установить 3', (setCount, count) => setCount(3)],
+  ['+1', (setCount, count) => setCount(count + 1)]
+]
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
+    <>
       <h1>Крутое WEB приложение</h1>
+      <hr />
+      <MyCounter count={count} />
+      <hr />
+      {btnContents.map((pair) => (
+        <MyBtn
+          key={uuidv4()}
+          label={pair[0]}
+          funcOnClick={() => pair[1](setCount, count)}
+        />
+      ))}
+    </>
   )
 }
 
---
+++ src/components/MyCounter.jsx
@@ -0,0 +1,6 @@
+
+export default function MyCounter({ count }) {
+  return <>
+    <h2>{count}</h2>
+  </>
+}
