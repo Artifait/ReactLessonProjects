b11301e3ec03395603fa1b7f74e19e470f6c0093
Artifait <am3044288@gmail.com>
2025-05-12 17:44:45 +0700

    End

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [info, setInfo] = useState("");
+  useEffect(() => {
+    fetch("https://api.exchangerate-api.com/v4/latest/USD")
+      .then((x) => x.json())
+      .then((info) => setInfo(info));
+  }, [])
+
+
   return (
+    <>
       <h1>Крутое WEB приложение</h1>
+      <hr />
+      {info && <h2>CAD - {info.rates.USD}</h2>}
+    </>
   )
 }
 
