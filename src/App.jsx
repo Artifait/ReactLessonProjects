be72ef954711c318fe2d6e6220e2d35788e6c422
Artifait <am3044288@gmail.com>
2025-05-21 17:26:06 +0700

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import Btn from './components/Btn'
 
 function App() {
+  const [key, setKey] = useState("Press Key");
+  const onKey = () => {
+
+  }
   return (
+    <>
       <h1>Крутое WEB приложение</h1>
+      <Btn text="dadsfs" onClick={() => console.log("DFSDF")} />
+    </>
   )
 }
 
---
+++ src/components/Btn.jsx
@@ -0,0 +1,8 @@
+import styles from './Btn.module.css';
+
+export default function Btn({ text, onClick, type = 'button' }) {
+  return (
+    <button className={styles.btn} onClick={onClick} type={type}>
+      {text}</button>
+  );
+};
